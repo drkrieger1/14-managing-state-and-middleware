9ffd8bb29fefe657ef067ef8f7b49c2f56c0ca4d
Erik Plyushko <eplyushko@gmail.com>
2017-08-31 14:12:19 -0700

    erik-joseph

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,9 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // COMMENT DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
   // (put your response in a comment here)
+  // its called when the user clicks the about link and that gets intersepted by routs.js which calls on thois ->then the aboutController.index is run and it will select the about element in the dom, then it will show the repoview.index and hid the siblings. request repo is in repos.js and the call back of repoView is repoview.js
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -4,8 +4,9 @@ var app = app || {};
 (function(module) {
   const articleController = {};
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // COMMENT DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
   // (put your response in a comment here)
+  // This function is being called in routes.js, and depending on which route it is being called from, it will load all the articles in the ctx object (if it was called from the '/' route, it would load every article). articleController.index calls app.articleView.index as a callback, and passes to it the ctx object -- which lives in articaleView.js
   articleController.index = (ctx) => app.articleView.index(ctx.articles);
 
   // REVIEW: Middleware for grabbing one article by ID:
@@ -15,8 +16,9 @@ var app = app || {};
       next();
     };
 
-    // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+    // COMMENT DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
     // (put your response in a comment here)
+    // its searching our database for a spesific article Id or whatever paremeters are passed to the function. Its calling article data and its located in articleController.
     app.Article.findWhere('article_id', ctx.params.article_id, articleData);
   };
 
